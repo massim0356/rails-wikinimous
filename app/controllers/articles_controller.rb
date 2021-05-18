32a3a209b89893ab93f86e7162c222a4579e1ff8
Massim Shah <massimshah@hotmail.com>
2021-05-18 15:43:46 +0900

    created models, views and routes

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,25 @@
+class ArticlesController < ApplicationController
+ 
+  def create
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
+end
